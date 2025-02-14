2faf9292023c465b25b766331c9c2ec115941424
Zoisit <smaragdeule@gmx.de>
2025-02-14 20:53:22 +0100

    C1-L3: Add input splitter

---
+++ input_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+    cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HoW  aRe YoU dOiNg?",
+			expected: []string{"how", "are", "you", "doing?"},
+		},
+		{
+			input:    " Charmander Bulbasaur PIKACHU ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Errorf("Expected %d strings in input, got %d", len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Expected %s, got %s", expectedWord, word)
+			}
+		}
+	}
+}
