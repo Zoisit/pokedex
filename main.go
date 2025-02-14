ebc3a4825ab094b4bc64fde302c0da51a31ac96b
Zoisit <smaragdeule@gmx.de>
2025-02-14 21:47:16 +0100

    C1-L4

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("Pokedex > ")
+		more := scanner.Scan()
+		if !more {
+			fmt.Print("...")
+		} else {
+			text := strings.TrimSpace(scanner.Text())
+			text = strings.ToLower(text)
+			inputs := strings.Fields(text)
+			fmt.Printf("Your command was: %s\n", inputs[0]) //careful, index out of bounds possible
+		}
+	}
 }
