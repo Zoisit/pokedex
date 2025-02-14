49801fdca7975f5d36461b490eca9d8e56ff59cd
Zoisit <smaragdeule@gmx.de>
2025-02-15 00:02:16 +0100

    C1-L5: Restructure

--- input_processing.go
+++ input_processing.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"bufio"
+	"os"
 	"strings"
 )
 
+func main_loop() {
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for true {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		inputs := cleanInput(scanner.Text())
+
+		cmd, ok := getCommands()[inputs[0]]
+		if ok {
+			cmd.callback()
+		} else {
+			fmt.Printf("Unknown command\n")
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.TrimSpace(text)
@@ -11,4 +31,19 @@ func cleanInput(text string) []string {
 	inputs := strings.Fields(text)
 
 	return inputs
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
--- main.go
+++ main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-	"strings"
-)
-
-
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	
-	commands := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-
-	for true {
-		fmt.Print("Pokedex > ")
-		more := scanner.Scan()
-		if !more {
-			fmt.Print("...")
-		} else {
-			text := strings.TrimSpace(scanner.Text())
-			text = strings.ToLower(text)
-			inputs := strings.Fields(text)
-
-			cmd, ok := commands[inputs[0]]
-			if ok {
-				cmd.callback()
-			} else {
-				fmt.Printf("Unknown command\n")
-			}
-		}
-	}
+	main_loop()
 }
 
 
